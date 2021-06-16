f642204c6b82a0154cdc2dde76a2e2e6c2038348
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2021-06-16 12:13:17 +0100

    Started TMVA

---
+++ TMVA_phase/bin/CMakeFiles/TMVA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TMVA.dir/TMVAClassification_federica.C.o"
+  "CMakeFiles/TMVA.dir/TMVAClassification_federica.C.o.d"
+  "TMVA"
+  "TMVA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TMVA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
