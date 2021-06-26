6eba14055b57ac27379cb33a3af998eb871a7f57
Lorenzo Principe <lorenzo.principe@cern.ch>
2021-06-26 23:10:04 +0200

    Added script to source environment variables in lxplus

---
+++ lx_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# If script is run, exit.
+[[ $_ == $0 ]] && (echo "Script is being run as executable. Please source script instead." && exit) || printf "\nSetting environment variables:\n\n" 
+
+export WORKSPACE_DIR="$PWD"
+export DATASETS_DIR="/eos/lhcb/user/f/feoliva/public"
+export TUTORIALS_DIR=$WORKSPACE_DIR"/tutorials"
+export CURRENT_DATASET=$DATASETS_DIR"/MC_production_XicStSt+/MC_Xi_DecFile26265970_Beam6500GeV-2016-MagDown-Nu1.6-25ns-Pythia8.root"
+
+
+printf "WORKSPACE_DIR\t=\t$WORKSPACE_DIR\n" 
+printf "DATASETS_DIR\t=\t$DATASETS_DIR\n" 
+printf "TUTORIALS_DIR\t=\t$TUTORIALS_DIR\n" 
+printf "CURRENT_DATASET\t=\t$CURRENT_DATASET\n"
