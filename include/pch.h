39c8205f63b6b608ff3dc156b34db52d5b6567ca
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2021-07-25 21:21:41 +0100

    Made pch.h available to all project

---
+++ include/pch.h
@@ -0,0 +1,29 @@
+#ifndef PCH
+#define PCH
+
+#include <cassert>
+#include <map>
+#include <memory>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#include "TString.h"
+#include "TTree.h"
+#include "TFile.h"
+#include "TROOT.h"
+#include "TRandom.h"
+#include "TCanvas.h"
+#include "TH1D.h"
+#include "TSystem.h"
+#include "TRint.h"
+#include "TApplication.h"
+#include "TStyle.h"
+#include "THStack.h"
+
+
+#include <nlohmann/json.hpp>
+
+#endif // PCH
