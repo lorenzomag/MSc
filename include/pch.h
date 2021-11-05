391148096d77214afc32fda35ce93f5b667c1b38
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2021-11-05 13:03:16 +0000

    Organise the include folder.

--- include/pch.h
+++ include/pch.h
@@ -1,34 +1,45 @@
 #ifndef PCH
 #define PCH
 
-#include <atomic>
+// #include <atomic>
 #include <cassert>
 #include <chrono>
 #include <fstream>
-#include <future>
+// #include <future>
 #include <iomanip>
 #include <iostream>
 #include <map>
 #include <memory>
-#include <mutex>
+// #include <mutex>
 #include <set>
-#include <thread>
+// #include <thread>
 #include <vector>
 
 #include "TApplication.h"
 #include "TCanvas.h"
+#include "TChain.h"
 #include "TFile.h"
 #include "TH1D.h"
 #include "THStack.h"
-#include "TROOT.h"
+#include "TLatex.h"
+#include "TObjString.h"
 #include "TRandom.h"
 #include "TRint.h"
+#include "TROOT.h"
 #include "TString.h"
 #include "TStyle.h"
 #include "TSystem.h"
 #include "TTree.h"
 
+#include "ROOT/RDataFrame.hxx"
 
-#include <nlohmann/json.hpp>
+#include "TMVA/Factory.h"
+#include "TMVA/DataLoader.h"
+#include "TMVA/Tools.h"
+#include "TMVA/OptimizeConfigParameters.h"
+#include "TMVA/TMVAGui.h"
+
+#include "BSlogger.hpp"
+#include "rang.hpp"
 
 #endif // PCH
