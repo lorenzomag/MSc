f7105bc17e70b5cc0f7ae3f76348fdb938dda55d
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2022-05-10 14:33:01 +0100

    Tidy up TMVAnalysis output canvases

--- TMVA/TMVAnalyse.C
+++ TMVA/TMVAnalyse.C
@@ -24,7 +24,7 @@
 using nlohmann::json;
 using ROOT::RDataFrame;
 
-void TMVAnalyse(const int FEATURE_SET = 9, const int HYPERPARAM_SET = 1)
+void TMVAnalyse(const int FEATURE_SET = 11, const int HYPERPARAM_SET = 1, std::string method = "BDTG", const double cut = 0.80)
 {
     ROOT::EnableImplicitMT();
 
@@ -37,16 +37,38 @@ void TMVAnalyse(const int FEATURE_SET = 9, const int HYPERPARAM_SET = 1)
 
     auto sig = ds.Filter("data_type == 0");
     auto bkg = ds.Filter("data_type == 1");
-    auto ds_app = ds.Filter("MVA_BDTG > 0.94").Filter("MassID == 1");
 
-        auto massHist = ds_app.Histo1D("Xicst_M");
-    auto sigHist = sig.Histo1D("Xicst_M");
-    auto bkgHist = bkg.Histo1D("Xicst_M");
+    auto ds_cut = ds.Filter(Form("MVA_%s > %f", method.c_str(), cut));
+    auto sig_cut = ds_cut.Filter("data_type == 0");
+    auto bkg_cut = ds_cut.Filter("data_type == 1");
 
-    // TCanvas *c1 = new TCanvas("c1","c3");
-    TCanvas *c2 = new TCanvas("c2", "c4");
+    ROOT::RDF::TH1DModel hist_model = {"","",200,2800,4000};
 
-    // massHist->DrawClone();
-    // sigHist->DrawClone();
+    auto massHist = ds.Histo1D(hist_model,"Xicst_M");
+    auto massHist_cut = ds_cut.Histo1D(hist_model,"Xicst_M");
+
+    auto sigHist = sig.Histo1D(hist_model,"Xicst_M");
+    auto sigHist_cut = sig_cut.Histo1D(hist_model,"Xicst_M");
+
+    auto bkgHist = bkg.Histo1D(hist_model,"Xicst_M");
+    auto bkgHist_cut = bkg_cut.Histo1D(hist_model,"Xicst_M");
+
+    TCanvas *c1 = new TCanvas("c1", "c1");
+    bkgHist->DrawClone();
+    bkgHist_cut->DrawClone("same");
+
+    TCanvas *c2 = new TCanvas("c2", "c2");
+    sigHist->DrawClone();
+    sigHist_cut->DrawClone("same");
+
+    TCanvas *c3 = new TCanvas("c3", "c3");
     massHist->DrawClone();
+    massHist_cut->DrawClone("same");
+
+    TCanvas *c4 = new TCanvas("c4","c4");
+    massHist_cut->DrawClone();
+    sigHist_cut->DrawClone("same");
+    bkgHist_cut->DrawClone("same");
+
+    
 }
