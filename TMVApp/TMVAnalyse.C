7a4dd3f476c7fb486be20187c5a509f263183789
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2022-05-17 11:06:14 +0100

    Add feature: choose ws to analyse

--- TMVApp/TMVAnalyse.C
+++ TMVApp/TMVAnalyse.C
@@ -33,21 +33,30 @@ using ROOT::RDataFrame;
 
 void SetBeforeAfterStyle(TH1 *before, TH1 *after);
 
-void TMVAnalyse(std::string method = "BDTG", const double cut = 0.84)
+void TMVAnalyse(const int ws = 0, std::string method = "BDTG", const double cut = 0.72)
 {
     ROOT::EnableImplicitMT();
 
-    TFile *file = new TFile(getenv("CURRENT_APPLICATION_DATASET"));
-    TTree *tree = (TTree *)file->Get("DecayTree");
+    // TFile *file = new TFile(getenv("CURRENT_APPLICATION_DATASET"));
+    // TTree *tree = (TTree *)file->Get("DecayTree");
+
+    TChain *tree = new TChain("DecayTree");
+    tree->Add(getenv("CURRENT_MC_DATASET"));
+    tree->Add(getenv("CURRENT_WS1_DATASET"));
+    tree->Add(getenv("CURRENT_WS2_DATASET"));
+    tree->Add(getenv("CURRENT_WS3_DATASET"));
 
     RDataFrame ds(*tree);
 
+    const char *ws_condition = ws == 0 ? "data_type == 1 || data_type == 2" : Form("data_type == %d", ws);
+    const char *ws_type = ws == 0 ? "WS1 + WS2" : Form("WS%d", ws);
+
     auto sig = ds.Filter("data_type == 0");
-    auto bkg = ds.Filter("data_type == 1");
+    auto bkg = ds.Filter(ws_condition);
 
     auto ds_cut = ds.Filter(Form("MVA_%s > %f", method.c_str(), cut));
     auto sig_cut = ds_cut.Filter("data_type == 0");
-    auto bkg_cut = ds_cut.Filter("data_type == 1");
+    auto bkg_cut = ds_cut.Filter(ws_condition);
 
     ROOT::RDF::TH1DModel hist_model = {"", "", 150, 2800, 4000};
 
@@ -83,15 +92,11 @@ void TMVAnalyse(std::string method = "BDTG", const double cut = 0.84)
     bkgHist_cut->SetLineStyle(1);
 
     TCanvas *c1 = new TCanvas("c1", "c1");
-    SetBeforeAfterStyle(bkgHist.GetPtr(),bkgHist_cut.GetPtr());
-    // c1->Divide(1,3);
-    // c1->cd(1);
+    SetBeforeAfterStyle(bkgHist.GetPtr(), bkgHist_cut.GetPtr());
 
-    // c1->cd(2);
-    // c1->cd(3);
-    THStack *hs_bkg = new THStack("WS1", "WS1");
-    // bkgHist->SetTitle("Before MVA");
-    // hs_bkg->Add(bkgHist.GetPtr());
+    THStack *hs_bkg = new THStack(ws_type, ws_type);
+    bkgHist->SetTitle("Before MVA");
+    hs_bkg->Add(bkgHist.GetPtr());
     bkgHist_cut->SetTitle("After MVA");
     hs_bkg->Add(bkgHist_cut.GetPtr());
     hs_bkg->DrawClone("nostack");
@@ -99,38 +104,92 @@ void TMVAnalyse(std::string method = "BDTG", const double cut = 0.84)
 
     TCanvas *c2 = new TCanvas("c2", "c2");
     THStack *hs_sig = new THStack("MC", "MC");
-    SetBeforeAfterStyle(sigHist.GetPtr(),sigHist_cut.GetPtr());
-    // sigHist->SetTitle("Before MVA");
-    // hs_sig->Add(sigHist.GetPtr());
+    SetBeforeAfterStyle(sigHist.GetPtr(), sigHist_cut.GetPtr());
+    sigHist->SetTitle("Before MVA");
+    hs_sig->Add(sigHist.GetPtr());
     sigHist_cut->SetTitle("After MVA");
     hs_sig->Add(sigHist_cut.GetPtr());
     hs_sig->DrawClone("nostack");
     c2->BuildLegend();
 
-    // TCanvas *c3 = new TCanvas("c3", "c3");
-    // THStack *hs_mass = new THStack("MC + WS1", "MC + WS1");
-    // SetBeforeAfterStyle(massHist.GetPtr(),massHist_cut.GetPtr());
-    // massHist->SetTitle("Before MVA");
-    // hs_mass->Add(massHist.GetPtr());
-    // massHist_cut->SetTitle("After MVA");
-    // hs_mass->Add(massHist_cut.GetPtr());
-    // hs_mass->DrawClone("nostack");
-    // c3->BuildLegend();
+    TCanvas *c3 = new TCanvas("c3", "c3");
+    THStack *hs_mass = new THStack(Form("MC + %s", ws_type), Form("MC + %s", ws_type));
+    SetBeforeAfterStyle(massHist.GetPtr(), massHist_cut.GetPtr());
+    massHist->SetTitle("Before MVA");
+    hs_mass->Add(massHist.GetPtr());
+    massHist_cut->SetTitle("After MVA");
+    hs_mass->Add(massHist_cut.GetPtr());
+    hs_mass->DrawClone("nostack");
+    c3->BuildLegend();
 
-    // TCanvas *c4 = new TCanvas("c4", "c4");
-    // THStack *hs_all = new THStack("After MVA", "After MVA");
-    // sigHist_cut->SetLineColor(9);
-    // bkgHist_cut->SetLineColor(8);
-    // massHist_cut->SetLineColor(2);
+    TCanvas *c4 = new TCanvas("c4", "c4");
+    THStack *hs_all = new THStack("After MVA", "After MVA");
+    sigHist_cut->SetLineColor(9);
+    bkgHist_cut->SetLineColor(8);
+    massHist_cut->SetLineColor(2);
 
-    // sigHist_cut->SetTitle("MC");
-    // bkgHist_cut->SetTitle("WS1");
-    // massHist_cut->SetTitle("MC+WS1");
-    // hs_all->Add(massHist_cut.GetPtr());
-    // hs_all->Add(sigHist_cut.GetPtr());
-    // hs_all->Add(bkgHist_cut.GetPtr());
-    // hs_all->DrawClone("nostack");
-    // c4->BuildLegend();
+    sigHist_cut->SetTitle("MC");
+    bkgHist_cut->SetTitle(ws_type);
+    massHist_cut->SetTitle(Form("MC + %s", ws_type));
+    hs_all->Add(massHist_cut.GetPtr());
+    hs_all->Add(sigHist_cut.GetPtr());
+    hs_all->Add(bkgHist_cut.GetPtr());
+    hs_all->DrawClone("nostack");
+    c4->BuildLegend();
+
+    if (ws == 0)
+    {
+        auto ws1 = bkg.Filter("data_type == 1");
+        auto ws2 = bkg.Filter("data_type == 2");
+        auto ws1_cut = bkg_cut.Filter("data_type == 1");
+        auto ws2_cut = bkg_cut.Filter("data_type == 2");
+
+        auto ws1_hist = ws1.Histo1D(hist_model, "Xicst_M");
+        auto ws2_hist = ws2.Histo1D(hist_model, "Xicst_M");
+        auto ws1_cut_hist = ws1_cut.Histo1D(hist_model, "Xicst_M");
+        auto ws2_cut_hist = ws2_cut.Histo1D(hist_model, "Xicst_M");
+
+
+        TCanvas *c5 = new TCanvas("c5", "c5");
+        THStack *hs_ws1 = new THStack("WS1", "WS1");
+        SetBeforeAfterStyle(ws1_hist.GetPtr(), ws1_cut_hist.GetPtr());
+        ws1_hist->SetTitle("Before MVA");
+        hs_ws1->Add(ws1_hist.GetPtr());
+        ws1_cut_hist->SetTitle("After MVA");
+        hs_ws1->Add(ws1_cut_hist.GetPtr());
+        hs_ws1->DrawClone("nostack");
+        c5->BuildLegend();
+
+        TCanvas *c6 = new TCanvas("c6", "c6");
+        THStack *hs_ws2 = new THStack("WS2", "WS2");
+        SetBeforeAfterStyle(ws2_hist.GetPtr(), ws2_cut_hist.GetPtr());
+        ws2_hist->SetTitle("Before MVA");
+        hs_ws2->Add(ws2_hist.GetPtr());
+        ws2_cut_hist->SetTitle("After MVA");
+        hs_ws2->Add(ws2_cut_hist.GetPtr());
+        hs_ws2->DrawClone("nostack");
+        c6->BuildLegend();
+
+        TCanvas *c7 = new TCanvas("c7", "c7");
+        THStack *hs_before = new THStack("Before Cut", "Before Cut");
+        SetBeforeAfterStyle(ws1_hist.GetPtr(), ws2_hist.GetPtr());
+        ws1_hist->SetTitle("WS1");
+        hs_before->Add(ws1_hist.GetPtr());
+        ws2_hist->SetTitle("WS2");
+        hs_before->Add(ws2_hist.GetPtr());
+        hs_before->DrawClone("nostack");
+        c7->BuildLegend();
+
+        TCanvas *c8 = new TCanvas("c8", "c8");
+        THStack *hs_after = new THStack("After Cut", "After Cut");
+        SetBeforeAfterStyle(ws1_cut_hist.GetPtr(), ws2_cut_hist.GetPtr());
+        ws1_cut_hist->SetTitle("WS1");
+        hs_after->Add(ws1_cut_hist.GetPtr());
+        ws2_cut_hist->SetTitle("WS2");
+        hs_after->Add(ws2_cut_hist.GetPtr());
+        hs_after->DrawClone("nostack");
+        c8->BuildLegend();
+    }
 }
 
 void SetBeforeAfterStyle(TH1 *before, TH1 *after)
